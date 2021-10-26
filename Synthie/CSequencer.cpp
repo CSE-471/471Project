f8e62f4411b8e362008a3377ab222b7f43f860b6
Tuxy222 <cdburnham18@gmai.com>
2021-10-26 13:32:46 -0400

    Sequencer XML work

--- Synthie/CSequencer.cpp
+++ Synthie/CSequencer.cpp
@@ -10,12 +10,17 @@ CSequencer::CSequencer()
     m_sampleRate = 44100.;
     m_samplePeriod = 1 / m_sampleRate;
     m_time = 0;
+
+    m_bpm = 120;
+    m_secperbeat = 0.5;
+    m_beatspermeasure = 4;
 }
+
 //! Start the synthesizer
 void CSequencer::Start(void)
 {
     m_time = 0;
-    m_synthesizers.clear();
+    //m_synthesizers.clear();
 }
 
 //! Generate one audio frame
@@ -50,10 +55,15 @@ bool CSequencer::Generate(double* frame)
         // Get a pointer to the allocated instrument
         CSynthesizer* synth = *node;
 
+        // Configure the instrument object
+        if (synth != NULL)
+        {
+            synth->SetSampleRate(GetSampleRate());
+        }
+
         // Call the generate function
         if (synth->Generate(frame))
         {
-            // True, so Synthesizer still active
         }
         else
         {
@@ -67,6 +77,7 @@ bool CSequencer::Generate(double* frame)
         node = next;
     }
 
+
     //
     // Phase 5: Determine when we are done
     //
@@ -78,6 +89,10 @@ bool CSequencer::Generate(double* frame)
 
 void CSequencer::Clear(void)
 {
+    for (CSynthesizer* synth : m_synthesizers)
+    {
+        delete(synth);
+    }
     m_synthesizers.clear();
 }
 
@@ -119,5 +134,131 @@ void CSequencer::OpenScore(CString& filename)
         // Get the name of the node
         CComBSTR nodeName;
         node->get_nodeName(&nodeName);
+
+        if (nodeName == "score")
+        {
+            XmlLoadScore(node);
+        }
     }
+}
+
+void CSequencer::XmlLoadScore(IXMLDOMNode* xml)
+{
+    // Get a list of all attribute nodes and the
+// length of that list
+    CComPtr<IXMLDOMNamedNodeMap> attributes;
+    xml->get_attributes(&attributes);
+    long len;
+    attributes->get_length(&len);
+
+    // Loop over the list of attributes
+    for (int i = 0; i < len; i++)
+    {
+        // Get attribute i
+        CComPtr<IXMLDOMNode> attrib;
+        attributes->get_item(i, &attrib);
+
+        // Get the name of the attribute
+        CComBSTR name;
+        attrib->get_nodeName(&name);
+
+        // Get the value of the attribute.  A CComVariant is a variable
+        // that can have any type. It loads the attribute value as a
+        // string (UNICODE), but we can then change it to an integer 
+        // (VT_I4) or double (VT_R8) using the ChangeType function 
+        // and then read its integer or double value from a member variable.
+        CComVariant value;
+        attrib->get_nodeValue(&value);
+
+        if (name == L"bpm")
+        {
+            value.ChangeType(VT_R8);
+            m_bpm = value.dblVal;
+            m_secperbeat = 1 / (m_bpm / 60);
+        }
+        else if (name == L"beatspermeasure")
+        {
+            value.ChangeType(VT_I4);
+            m_beatspermeasure = value.intVal;
+        }
+
+    }
+
+
+    CComPtr<IXMLDOMNode> node;
+    xml->get_firstChild(&node);
+    for (; node != NULL; NextNode(node))
+    {
+        // Get the name of the node
+        CComBSTR name;
+        node->get_nodeName(&name);
+
+        if (name == L"synthesizer")
+        {
+            XmlLoadSynthesizer(node);
+        }
+    }
+}
+
+void CSequencer::XmlLoadSynthesizer(IXMLDOMNode* xml)
+{
+    wstring synthesizer = L"";
+
+    // Get a list of all attribute nodes and the
+    // length of that list
+    CComPtr<IXMLDOMNamedNodeMap> attributes;
+    xml->get_attributes(&attributes);
+    long len;
+    attributes->get_length(&len);
+
+    // Loop over the list of attributes
+    for (int i = 0; i < len; i++)
+    {
+        // Get attribute i
+        CComPtr<IXMLDOMNode> attrib;
+        attributes->get_item(i, &attrib);
+
+        // Get the name of the attribute
+        CComBSTR name;
+        attrib->get_nodeName(&name);
+
+        // Get the value of the attribute.  
+        CComVariant value;
+        attrib->get_nodeValue(&value);
+
+        if (name == "synthesizer")
+        {
+            synthesizer = value.bstrVal;
+        }
+
+    }
+
+
+    CComPtr<IXMLDOMNode> node;
+    xml->get_firstChild(&node);
+    for (; node != NULL; NextNode(node))
+    {
+        // Get the name of the node
+        CComBSTR name;
+        node->get_nodeName(&name);
+
+        if (name == L"instrument")
+        {
+            if (synthesizer == L"Additive")
+            {
+                CSynthesizer* synth = new CSynthesizer(m_bpm, m_beatspermeasure, m_secperbeat);
+                m_synthesizers.push_back(synth); // Make this Cadditive once that's setup
+                m_synthesizers.back()->XmlLoadInstrument(node);
+                m_synthesizers.back()->Start();
+            }
+            else
+            {
+                m_synthesizers.push_back(&CSynthesizer());
+                m_synthesizers.back()->XmlLoadInstrument(node);
+                m_synthesizers.back()->Start();
+            }
+        }
+
+    }
+
 }
